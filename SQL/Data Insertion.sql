bbbbd046cdf02c41d648509edb823ef00308720f
rehaan <md.rehaan.shaikh2@gmail.com>
2026-05-13 20:49:40 +0530

    Refactor SQL data insertion script to use standardized COPY format with encoding

--- SQL/Data Insertion.sql
+++ SQL/Data Insertion.sql
@@ -1,6 +1,10 @@
 SET datestyle = 'ISO, DMY';
 
 COPY patient_visits
-FROM 'C:\IDE\Projects\hospital-Operations-Analytics\Data\cleaned_patient_data.csv'
-DELIMITER ','
-CSV HEADER;
+FROM 'C:/IDE/Projects/hospital-Operations-Analytics/Data/cleaned_patient_data.csv'
+WITH (
+    FORMAT CSV,
+    HEADER,
+    DELIMITER ',',
+    ENCODING 'UTF8'
+);
